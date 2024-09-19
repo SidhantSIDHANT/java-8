93194c34d7b334e5d88df2710d3261c9a0d8afda
SidhantSIDHANT <sidhantkulkarni27@gmail.com>
2024-09-19 18:00:30 +0530

    add constructor to add Element in list

--- src/main/java/org/java8/App.java
+++ src/main/java/org/java8/App.java
@@ -2,11 +2,15 @@ package org.java8;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
 
     List<Employee> employeeList = new ArrayList<Employee>();
+    List<Integer> arrList = new ArrayList();
 
     class Employee {
         String empName;
@@ -46,10 +50,8 @@ public class App {
 
         Map<String, List<Employee>> groupingByConcurrent = employeeList.stream().collect(Collectors.groupingByConcurrent((Employee element) -> {
             return element.empAddress;
-            //
         }, () -> {
             return new ConcurrentHashMap<>();
-            //
         }, Collectors.toList()));
         System.out.println(groupingByConcurrent);
 
@@ -69,10 +71,10 @@ public class App {
         System.out.println(mappingWithInteger);
 //
         Map<Integer, List<Employee>> mapElement = employeeList.stream().collect(Collectors.toMap(element -> element.empId, element2 -> {
+            return Collections.singletonList(element2);
 //            List<Employee> list = new ArrayList<Employee>();
 //            list.add(element2);
 //            return list;
-            return Collections.singletonList(element2);
         }));
         System.out.println(mapElement);
 
@@ -82,9 +84,70 @@ public class App {
             return min1.compareTo(min2);
         }));
         System.out.println(optional);
+
+        Map<Boolean, List<Integer>> partitioningBy = employeeList.stream().collect(Collectors.partitioningBy((element) -> {
+            return element.empId >= 100;
+        }, Collectors.mapping(element -> element.empId, Collectors.toList())));
+        System.out.println(partitioningBy);
+
+        Map<Boolean, Map<Integer, Employee>> partitioningByMap = employeeList.stream().collect(Collectors.partitioningBy(predicate -> predicate.empId <= 100, Collectors.toMap(key -> key.empId, value -> value)));
+        System.out.println(partitioningByMap);
+
+        arrList.add(20);
+        arrList.add(30);
+        arrList.add(40);
+        arrList.add(55);
+        Optional<Integer> optional23 = arrList.stream().collect(Collectors.reducing((ele, ele2) -> {
+            return ele + ele2;
+        }));
+        System.out.println(optional23);
+
+        Optional<Employee> optional12 = employeeList.stream().collect(Collectors.reducing(new BinaryOperator<Employee>() {
+            public Employee apply(Employee a, Employee b) {
+                return a;
+            }
+        }));
+        System.out.println(optional12);
+
+        Optional<Employee> optional123 = employeeList.stream().collect(Collectors.reducing((Employee emp, Employee emp12) -> {
+            return emp;
+        }));
+
+        Optional<Integer> returnOptional = arrList.stream().collect(Collectors.reducing((reduce1, reduce2) -> {
+            return reduce1 - reduce2;
+        }));
+        System.out.println(returnOptional);
+
+        //summarizingLong
+        LongSummaryStatistics summrizingToLong = arrList.stream().collect(Collectors.summarizingLong((Integer integer) -> {
+            return integer;
+        }));
+        System.out.println(summrizingToLong);
+
     }
 
     public void streamMethod() {
+        Random random = new Random();
+        Stream.generate(random::nextInt).limit(4).forEach(System.out::println);
+        this.employeeList.stream().forEachOrdered(element -> {
+            System.out.println(element);
+        });
+
+        System.out.println("-----------------------------------------------------------------------------");
+
+        this.employeeList.stream().forEach(item -> {
+            System.out.println(item);
+        });
+        List<Integer> linkedList = new LinkedList<Integer>();
+        linkedList.add(10);
+        linkedList.add(50);
+        linkedList.add(20);
+        linkedList.add(30);
+        List<Integer> mappedData = linkedList.stream().peek(System.out::println).map(element -> element * 2).collect(Collectors.toList());
+        System.out.println(linkedList);
+        List<Integer> sorted = linkedList.stream().sorted().collect(Collectors.toList());
+        System.out.println(sorted);
+
 
     }
 
