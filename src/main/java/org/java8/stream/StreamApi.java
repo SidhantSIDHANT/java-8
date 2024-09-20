b9011c1afa234f01e61d276bc03015428abc827f
SidhantSIDHANT <sidhantkulkarni27@gmail.com>
2024-09-20 10:56:55 +0530

    working on changes

--- src/main/java/org/java8/stream/StreamApi.java
+++ src/main/java/org/java8/stream/StreamApi.java
@@ -1,10 +1,10 @@
 package org.java8.stream;
 
-import java.security.KeyStore;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class PredicateDemo implements Predicate<Integer> {
     @Override
@@ -66,6 +66,10 @@ class BinaryOperatorDemo implements BinaryOperator<String> {
 }
 
 public class StreamApi {
+
+    List<StreamApi.Employee> employeeList = new ArrayList<StreamApi.Employee>();
+    List<Integer> arrList = new ArrayList();
+
 
     public void binaryOperator() {
         BinaryOperator<String> binaryOperatorAnonymousFunction = new BinaryOperator<String>() {
@@ -198,8 +202,8 @@ public class StreamApi {
     public void streamMethods() {
         Employee emp1 = new Employee("java", "java Programming", 22, 123456789);
         Employee emp2 = new Employee("javaScript", "javaScript Programming", 01, 010101010101);
-        Employee emp3 = new Employee("Phython", "Phython Programming", 0365, 55555555555555l);
-        Employee emp4 = new Employee("Html", "Html Programming", 22, 125468896545l);
+        Employee emp3 = new Employee("Phython", "Phython Programming", 0365, 55555555555555L);
+        Employee emp4 = new Employee("Html", "Html Programming", 22, 125468896545L);
         List<Employee> employeeList = new ArrayList<>();
         employeeList.add(emp1);
         employeeList.add(emp2);
@@ -215,7 +219,7 @@ public class StreamApi {
         arrList.add(10);
 
         double getDoubleValue = arrList.stream().collect(Collectors.averagingInt(element -> {
-            return new Integer(element).intValue();
+            return element.intValue();
         }));
         System.out.println(getDoubleValue);
 
@@ -237,17 +241,130 @@ public class StreamApi {
 
         Map<Integer, List<Employee>> employeeMap2 = employeeList.stream().collect(Collectors.groupingBy(groupElement -> groupElement.empId));
 
+        Random random = new Random();
+        Stream.generate(random::nextInt).limit(4).forEach(System.out::println);
+        this.employeeList.stream().forEachOrdered(element -> {
+            System.out.println(element);
+        });
+
+        System.out.println("-----------------------------------------------------------------------------");
+
+        this.employeeList.stream().forEach(item -> {
+            System.out.println(item);
+        });
+        List<Integer> linkedList = new LinkedList<Integer>();
+        linkedList.add(10);
+        linkedList.add(50);
+        linkedList.add(20);
+        linkedList.add(30);
+        List<Integer> mappedData = linkedList.stream().peek(System.out::println).map(element -> element * 2).collect(Collectors.toList());
+        System.out.println(linkedList);
+        List<Integer> sorted = linkedList.stream().sorted().collect(Collectors.toList());
+        System.out.println(sorted);
+    }
+
+    public void collectrosMethod() {
+        StreamApi.Employee emp1 = new StreamApi().new Employee("java", "mumbai", 15, 2222222222L);
+        StreamApi.Employee emp2 = new StreamApi().new Employee("phython", "pune", 25, 123456789L);
+        StreamApi.Employee emp3 = new StreamApi().new Employee("javaScript", "Latur", 35, 3333333333L);
+        StreamApi.Employee emp4 = new StreamApi().new Employee("Hari Om", "Banner", 2566541, 78945612300L);
+        employeeList.add(emp1);
+        employeeList.add(emp2);
+        employeeList.add(emp3);
+        employeeList.add(emp4);
+
+        Map<Integer, List<StreamApi.Employee>> map = employeeList.stream().collect(Collectors.groupingBy((item) -> {
+            return item.empId;
+        }, () -> {
+            return new HashMap<Integer, List<StreamApi.Employee>>();
+        }, Collectors.toList()));
+        System.out.println(map);
+
+        Map<String, List<StreamApi.Employee>> groupingByConcurrent = employeeList.stream().collect(Collectors.groupingByConcurrent((StreamApi.Employee element) -> {
+            return element.empAddress;
+        }, () -> {
+            return new ConcurrentHashMap<>();
+        }, Collectors.toList()));
+        System.out.println(groupingByConcurrent);
+
+        String joining = employeeList.stream().map(element -> {
+            return element.empName;
+        }).collect(Collectors.joining(""));
+        System.out.println(joining);
+
+        Map<Integer, List<Integer>> mapping = employeeList.stream().collect(Collectors.groupingBy(element -> element.empId, Collectors.mapping(maping -> maping.empId, Collectors.toList())));
+
+        List<String> mappingTheElement = employeeList.stream().collect(Collectors.mapping(element -> element.empAddress, Collectors.toList()));
+        System.out.println(mappingTheElement);
+
+        List<Integer> mappingWithInteger = employeeList.stream().collect(Collectors.mapping(element -> element.empId, Collectors.toList()));
+        System.out.println(mappingWithInteger);
+        //
+        Map<Integer, List<StreamApi.Employee>> mapElement = employeeList.stream().collect(Collectors.toMap(element -> element.empId, element2 -> {
+            return Collections.singletonList(element2);
+            // List<Employee> list = new ArrayList<Employee>();
+            // list.add(element2);
+            // return list;
+        }));
+        System.out.println(mapElement);
+
+        List<Integer> mapElement2 = employeeList.stream().collect(Collectors.mapping(mapped -> mapped.empId, Collectors.toList()));
+
+        Optional<Integer> optional = mapElement2.stream().collect(Collectors.minBy((min1, min2) -> {
+            return min1.compareTo(min2);
+        }));
+        System.out.println(optional);
+
+        Map<Boolean, List<Integer>> partitioningBy = employeeList.stream().collect(Collectors.partitioningBy((element) -> {
+            return element.empId >= 100;
+        }, Collectors.mapping(element -> element.empId, Collectors.toList())));
+        System.out.println(partitioningBy);
+
+        Map<Boolean, Map<Integer, StreamApi.Employee>> partitioningByMap = employeeList.stream().collect(Collectors.partitioningBy(predicate -> predicate.empId <= 100, Collectors.toMap(key -> key.empId, value -> value)));
+        System.out.println(partitioningByMap);
+
+        arrList.add(20);
+        arrList.add(30);
+        arrList.add(40);
+        arrList.add(55);
+        Optional<Integer> optional23 = arrList.stream().collect(Collectors.reducing((ele, ele2) -> {
+            return ele + ele2;
+        }));
+        System.out.println(optional23);
+
+        Optional<StreamApi.Employee> optional12 = employeeList.stream().collect(Collectors.reducing(new BinaryOperator<StreamApi.Employee>() {
+            public StreamApi.Employee apply(StreamApi.Employee a, StreamApi.Employee b) {
+                return a;
+            }
+        }));
+        System.out.println(optional12);
+
+        Optional<StreamApi.Employee> optional123 = employeeList.stream().collect(Collectors.reducing((StreamApi.Employee emp, StreamApi.Employee emp12) -> {
+            return emp;
+        }));
+
+        Optional<Integer> returnOptional = arrList.stream().collect(Collectors.reducing((reduce1, reduce2) -> {
+            return reduce1 - reduce2;
+        }));
+        System.out.println(returnOptional);
+
+        // summarizingLong
+        LongSummaryStatistics summrizingToLong = arrList.stream().collect(Collectors.summarizingLong((Integer integer) -> {
+            return integer;
+        }));
+        System.out.println(summrizingToLong);
+
     }
 
     public static void main(String[] arg) {
         StreamApi streamApi = new StreamApi();
-//        streamApi.binaryOperator();
-//        streamApi.unaryOperator();
-//        streamApi.predicate();
-//        streamApi.function();
-//        streamApi.consumer();
-//        streamApi.supplier();
+        streamApi.binaryOperator();
+        streamApi.unaryOperator();
+        streamApi.predicate();
+        streamApi.function();
+        streamApi.consumer();
+        streamApi.supplier();
         streamApi.streamMethods();
-
+        streamApi.collectrosMethod();
     }
 }
